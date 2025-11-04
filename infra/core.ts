35b19855f395de5c7a040a1698f124ae6fcf9854
Sultan Valiyev <60121419+sultanvaliyev@users.noreply.github.com>
2025-11-04 10:39:48 -0800

    remove opennext dependency

--- infra/core.ts
+++ infra/core.ts
@@ -3,4 +3,6 @@ export const coreFunction = new sst.aws.Function('PythonFunctio', {
   runtime: 'python3.11',
   url: true,
   bundle: 'packages/functions/core',
+  timeout: '30 seconds',
+  memory: '512 MB',
 });
--- infra/web.ts
+++ infra/web.ts
@@ -1,6 +1,5 @@
 export const web = new sst.aws.Nextjs('Web', {
   path: 'packages/web',
-  buildCommand: 'NODE_ENV=production npm run build',
   environment: {
     NODE_ENV: 'production',
   },
